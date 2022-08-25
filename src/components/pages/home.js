9ceb1dbf15e4d54a14742f26842c4273f07019a7
waterburner <Waterburner@users.noreply.github.com>
2022-08-25 18:33:13 -0500

    changed formatting (no biggie)

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,44 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-export default function() {
+export default function () {
     return (
         <div className="home-wrapper">
             <div className="cover-img">
                 {/* <img src="../../static/assets/img/banner-06.jpg" alt="banner cover photo"/> */}
-                <img src="https://thesunflowerbakeryandcafe.com/wp-content/uploads/2020/09/banner-06.jpg" alt="banner cover photo"/>
+                <img
+                    src="https://thesunflowerbakeryandcafe.com/wp-content/uploads/2020/09/banner-06.jpg"
+                    alt="banner cover photo"
+                />
             </div>
 
             <div className="now-offering-section">
-                    <h2 className="now-offering">Now Offering Dine-In, Pick-Up & Curbside Service At <br />The Sunflower</h2>
+                <h2 className="now-offering">
+                    Now Offering Dine-In, Pick-Up & Curbside Service At <br />
+                    The Sunflower
+                </h2>
                 <div className="offer">
                     <div className="offer-left-side">
                         <div className="picture">
-                            <img src="https://thesunflowerbakeryandcafe.com/wp-content/uploads/2019/11/frame-square-02.jpg" alt=""/>
+                            <img
+                                src="https://thesunflowerbakeryandcafe.com/wp-content/uploads/2019/11/frame-square-02.jpg"
+                                alt=""
+                            />
                         </div>
                     </div>
 
                     <div className="offer-right-side">
-
                         <div className="offer-right-side-postwrapper">
-
                             <div className="header-section">
-                                <h2>Our Full Menu For Breakfast & Lunch, & Weekday Lunch Specials!</h2>
+                                <h2>
+                                    Our Full Menu For Breakfast & Lunch, &
+                                    Weekday Lunch Specials!
+                                </h2>
                             </div>
                             <hr />
                             <div className="contact-section">
-                                <h3><b>Curbside pick-up or take-out available! You can even get mixed drinks & cappuccino to-go!</b></h3>
+                                <h3>
+                                    <b>
+                                        Curbside pick-up or take-out available!
+                                        You can even get mixed drinks &
+                                        cappuccino to-go!
+                                    </b>
+                                </h3>
                                 <div className="contacts-wrapper">
                                     <div className="contacts-left">
                                         <p>Curbside:</p>
                                     </div>
                                     <div className="contacts-right">
-                                        <p className="phone-number">(409) 763-5500</p>
-                                        <p className="phone-number">(409) 765-0040</p>
+                                        <p className="phone-number">
+                                            (409) 763-5500
+                                        </p>
+                                        <p className="phone-number">
+                                            (409) 765-0040
+                                        </p>
                                     </div>
                                 </div>
-                                <p>Please support our staff with generous tips & your thanks for their hard work!</p>
+                                <p>
+                                    Please support our staff with generous tips
+                                    & your thanks for their hard work!
+                                </p>
                             </div>
-
                         </div>
                     </div>
                 </div>
@@ -46,49 +68,67 @@ export default function() {
 
             <div className="three-button-section-wrapper">
                 <div className="three-button-section">
-
                     <div className="row-wrapper">
-                        <div className="picture-row">
+                        <div className="left">
                             <div className="picture-wrapper">
                                 {/* <div className="picture picture-left"></div> */}
-                                <a href="#"><img className="picture" src="https://thesunflowerbakeryandcafe.com/wp-content/uploads/2019/10/frame-vertical-pancake.jpg" alt="image"/>
+                                <a href="#">
+                                    <img
+                                        className="picture"
+                                        src="https://thesunflowerbakeryandcafe.com/wp-content/uploads/2019/10/frame-vertical-pancake.jpg"
+                                        alt="image"
+                                    />
                                 </a>
                             </div>
-                            
+                            <div className="button">
+                                <a>
+                                    <button className="button-hover">
+                                        Breakfast
+                                    </button>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="center">
                             <div className="picture-wrapper">
                                 {/* <div className="picture picture-center"></div> */}
-                                <a href="#"><img className="picture" src="https://thesunflowerbakeryandcafe.com/wp-content/uploads/2019/10/frame-vertical-fish-tacos.jpg" alt="image"/>
+                                <a href="#">
+                                    <img
+                                        className="picture"
+                                        src="https://thesunflowerbakeryandcafe.com/wp-content/uploads/2019/10/frame-vertical-fish-tacos.jpg"
+                                        alt="image"
+                                    />
                                 </a>
                             </div>
-                            
+                            <div className="button">
+                                <a>
+                                    <button className="button-hover">
+                                        Lunch
+                                    </button>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="right">
                             <div className="picture-wrapper">
                                 {/* <div className="picture picture-right"></div> */}
-                                <a href="#"><img className="picture" src="https://thesunflowerbakeryandcafe.com/wp-content/uploads/2019/10/frame-vertical-chocolate-cheesecake.jpg" alt="image"/>
+                                <a href="#">
+                                    <img
+                                        className="picture"
+                                        src="https://thesunflowerbakeryandcafe.com/wp-content/uploads/2019/10/frame-vertical-chocolate-cheesecake.jpg"
+                                        alt="image"
+                                    />
+                                </a>
+                            </div>
+                            <div className="button">
+                                <a>
+                                    <button className="button-hover">
+                                        Out Story
+                                    </button>
                                 </a>
                             </div>
                         </div>
                     </div>
-                    <div className="row-wrapper">
-                        <div className="button-row">
-                            <div className="button">
-                                <a><button className="button-hover">Breakfast</button>
-                                </a>
-                            </div>
-
-                            <div className="button">
-                                <a><button className="button-hover">Lunch</button>
-                                </a>
-                            </div>
-                            
-                            <div className="button">
-                                <a><button className="button-hover">Out Story</button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-
-
                 </div>
             </div>
 
@@ -114,7 +154,11 @@ export default function() {
                                 </div>
                             </div>
                             <div className="specials-menu-description">
-                                <p>Fried Quail Over Homemade Cheese Grits With Spinach, Tomatoes, And Butter Sauce. Served With Jalapeno Toast. </p>
+                                <p>
+                                    Fried Quail Over Homemade Cheese Grits With
+                                    Spinach, Tomatoes, And Butter Sauce. Served
+                                    With Jalapeno Toast.{" "}
+                                </p>
                             </div>
                         </div>
                         <div className="specials-menu-item">
@@ -128,7 +172,12 @@ export default function() {
                                 </div>
                             </div>
                             <div className="specials-menu-description">
-                                <p>Three Egg Omelet Filled With Spicy Roasted Tomatoes And Serrano Salsa, Topped With Guacamole And Queso Fresco Served With Black Beans And Breakfast Potatoes </p>
+                                <p>
+                                    Three Egg Omelet Filled With Spicy Roasted
+                                    Tomatoes And Serrano Salsa, Topped With
+                                    Guacamole And Queso Fresco Served With Black
+                                    Beans And Breakfast Potatoes{" "}
+                                </p>
                             </div>
                         </div>
 
@@ -143,7 +192,11 @@ export default function() {
                                 </div>
                             </div>
                             <div className="specials-menu-description">
-                                <p>Homemade Belgian Waffle. Topped With Whipped Cream, And Berries, And A Generous Portion Of Meyer Lemon Curd.</p>
+                                <p>
+                                    Homemade Belgian Waffle. Topped With Whipped
+                                    Cream, And Berries, And A Generous Portion
+                                    Of Meyer Lemon Curd.
+                                </p>
                             </div>
                         </div>
                         <div className="specials-menu-item">
@@ -172,7 +225,11 @@ export default function() {
                                 </div>
                             </div>
                             <div className="specials-menu-description">
-                                <p>Grilled Flat Iron Steak With Three Eggs Any Style. Served With Breakfast Potatoes And Toast</p>
+                                <p>
+                                    Grilled Flat Iron Steak With Three Eggs Any
+                                    Style. Served With Breakfast Potatoes And
+                                    Toast
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -206,7 +263,6 @@ export default function() {
                     </div>
                 </div>
             </div>
-
         </div>
     );
 }
