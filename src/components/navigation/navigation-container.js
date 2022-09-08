112a0f4a0c954fd5a70b53cd084804339d0d9fa3
waterburner <Waterburner@users.noreply.github.com>
2022-09-08 00:16:10 -0500

    corrected for responsive design

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -21,63 +21,65 @@ const NavigationComponent = (props) => {
                         <img src="https://thesunflowerbakeryandcafe.com/wp-content/uploads/2019/11/logo-newest-200.jpg" />
                     </div>
                     <div className="nav-links">
-                        <div className="burger">=</div>
-                        <div className="nav-link-wrapper">
-                            <NavLink
-                                exact
-                                to="/"
-                                className="nav-link"
-                                activeClassName="nav-link-active"
-                            >
-                                Home
-                            </NavLink>
-                        </div>
-                        <div className="nav-link-wrapper">
-                            <NavLink
-                                to="/about"
-                                className="nav-link"
-                                activeClassName="nav-link-active"
-                            >
-                                About
-                            </NavLink>
-                        </div>
-                        <div className="nav-link-wrapper">
-                            <NavLink
-                                to="/menu"
-                                className="nav-link"
-                                activeClassName="nav-link-active"
-                            >
-                                Menu
-                            </NavLink>
-                        </div>
-                        <div className="nav-link-wrapper">
-                            {/* <NavLink to="/directions" className="nav-link" activeClassName="nav-link-active"> */}
-                            <a
-                                href="https://www.google.com/maps/dir/29.4003391,-94.9507061/sunflower+bakery+and+cafe/@29.3466329,-94.9396674,12z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x863f9e471b022a11:0xf8e605ae95bd1c2d!2m2!1d-94.7830248!2d29.3070635"
-                                className="nav-link"
-                                activeClassName="nav-link-active"
-                            >
-                                Directions
-                            </a>
-                            {/* </NavLink> */}
-                        </div>
-                        <div className="nav-link-wrapper">
-                            {/* <NavLink to="/contact" className="nav-link" activeClassName="nav-link-active"> */}
-                            <a
-                                href="https://thesunflowerbakeryandcafe.com/contact-sunflower-bakery-and-cafe-galveston-tx/"
-                                className="nav-link"
-                                activeClassName="nav-link-active"
-                            >
-                                Contact
-                            </a>
-                            {/* </NavLink> */}
-                        </div>
-                        <div className="nav-socials-wrapper">
-                            <div className="social-buttons">
-                                <div className="social-media-btn">FB</div>
-                                <div className="social-media-btn">IG</div>
-                                <div className="social-media-btn">TA</div>
-                                <div className="social-media-btn">TT</div>
+                        <div className="burger">
+                            =
+                            <div className="nav-link-wrapper">
+                                <NavLink
+                                    exact
+                                    to="/"
+                                    className="nav-link"
+                                    activeClassName="nav-link-active"
+                                >
+                                    Home
+                                </NavLink>
+                                <NavLink
+                                    to="/about"
+                                    className="nav-link"
+                                    activeClassName="nav-link-active"
+                                >
+                                    About
+                                </NavLink>
+                                <NavLink
+                                    to="/menu"
+                                    className="nav-link"
+                                    activeClassName="nav-link-active"
+                                >
+                                    Menu
+                                </NavLink>
+                                {/* <NavLink to="/directions" className="nav-link" activeClassName="nav-link-active"> */}
+                                <a
+                                    href="https://www.google.com/maps/dir/29.4003391,-94.9507061/sunflower+bakery+and+cafe/@29.3466329,-94.9396674,12z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x863f9e471b022a11:0xf8e605ae95bd1c2d!2m2!1d-94.7830248!2d29.3070635"
+                                    className="nav-link"
+                                    activeClassName="nav-link-active"
+                                >
+                                    Directions
+                                </a>
+                                {/* </NavLink> */}
+                                {/* <NavLink to="/contact" className="nav-link" activeClassName="nav-link-active"> */}
+                                <a
+                                    href="https://thesunflowerbakeryandcafe.com/contact-sunflower-bakery-and-cafe-galveston-tx/"
+                                    className="nav-link"
+                                    activeClassName="nav-link-active"
+                                >
+                                    Contact
+                                </a>
+                                {/* </NavLink> */}
+                                <div className="nav-socials-wrapper">
+                                    <div className="social-buttons">
+                                        <div className="social-media-btn">
+                                            FB
+                                        </div>
+                                        <div className="social-media-btn">
+                                            IG
+                                        </div>
+                                        <div className="social-media-btn">
+                                            TA
+                                        </div>
+                                        <div className="social-media-btn">
+                                            TT
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
